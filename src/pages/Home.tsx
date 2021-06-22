fc73da6682fbb8df3f7cddcd5348ae4ef9138671
MarlonDener <m.dener01@hotmail.com>
2021-06-22 02:20:44 -0300

    Pages and Components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,47 @@
+import { useHistory } from "react-router-dom";
+import ilustrationImg from "../assets/images/ilustraImg.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
+import { Button } from "../components/Button";
+import "../styles/auth.scss";
+
+export const Home = () => {
+  //History is a function for navigate without ahref
+  const history = useHistory();
+  function navigateToNewRoom() {
+    history.push("/rooms/new");
+  }
+
+  function handleCreateRoom() {
+    history.push("/rooms/new");
+  }
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={ilustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie sals de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="TheBestQuestion" />
+          <button className="create-row" onClick={navigateToNewRoom}>
+            <img src={googleIconImg} alt="logo do google" />
+            Crie sua sala com o Google
+          </button>
+
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entra na sala</Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
